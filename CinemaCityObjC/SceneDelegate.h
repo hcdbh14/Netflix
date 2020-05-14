4bbfd089b41f6428fee092a48f41771df5ffb404
Kenny Kurochkin <kennnkuro15@gmail.com>
2020-05-14 17:05:16 +0300

    Initial Commit

---
+++ CinemaCityObjC/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  CinemaCityObjC
+//
+//  Created by Kenny Kurochkin on 14/05/2020.
+//  Copyright © 2020 Kenny Kurochkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ CinemaCityObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CinemaCityObjC
+//
+//  Created by Kenny Kurochkin on 14/05/2020.
+//  Copyright © 2020 Kenny Kurochkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
